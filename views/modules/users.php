455e949393c9b517c4c8d533b494073f3d713739
Arturo R <ircsasw@gmail.com>
2018-07-16 19:42:48 -0500

    tabla de usuarios

--- views/modules/users.php
+++ views/modules/users.php
@@ -5,14 +5,89 @@
       Administrar usiarios
     </h1>
     <ol class="breadcrumb">
-      <li><a href="#"><i class="fa fa-dashboard"></i> Inicio</a></li>
+      <li><a href="home"><i class="fa fa-dashboard"></i> Inicio</a></li>
       <li class="active">Administrar usuarios</li>
     </ol>
   </section>
 
   <!-- Main content -->
   <section class="content">
+    <div class="box">
 
+      <div class="box-header with-border">
+
+        <button class="btn btn-primary" data-toggle="modal" data-target="#modalAgregarUsuario">
+          Agregar usuario
+        </button>
+
+      </div>
+
+      <div class="box-body">
+
+        <table class="table table-bordered table-striped">
+          <thead>
+          <tr>
+            <th style="width:10px">#</th>
+            <th>Nombre</th>
+            <th>Usuario</th>
+            <th>Foto</th>
+            <th>Perfil</th>
+            <th>Estado</th>
+            <th>Último login</th>
+            <th>Acciones</th>
+            </tr>
+          </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Usuario Administrado</td>
+            <td>admin</td>
+            <td><img src="views/img/users/default/anonymous.png" alt=""></td>
+            <td>Administrador</td>
+            <td><button class="btn btn-success btn-xs">Activado</button></td>
+            <td>2017-12-11 12:05:32</td>
+            <td>
+              <div class="btn-group">
+                <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+                <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>Usuario Administrado</td>
+            <td>admin</td>
+            <td><img src="views/img/users/default/anonymous.png" alt=""></td>
+            <td>Administrador</td>
+            <td><button class="btn btn-success btn-xs">Activado</button></td>
+            <td>2017-12-11 12:05:32</td>
+            <td>
+              <div class="btn-group">
+                <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+                <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>Usuario Administrado</td>
+            <td>admin</td>
+            <td><img src="views/img/users/default/anonymous.png" alt=""></td>
+            <td>Administrador</td>
+            <td><button class="btn btn-success btn-xs">Activado</button></td>
+            <td>2017-12-11 12:05:32</td>
+            <td>
+              <div class="btn-group">
+                <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+                <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+        </table>
+
+      </div>
+    </div>
   </section>
   <!-- /.content -->
 </div>
